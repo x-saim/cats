beea11dbbb0790720249d71ef0d338cd5f9a9423
Saim Amir <saimxamir@gmail.com>
2023-03-20 17:28:14 -0400

    Async functions call back with data

--- syncBreeds.js
+++ syncBreeds.js
@@ -1,16 +1,17 @@
 const fs = require("fs");
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, funcWhenThingsDone) {
   console.log("breedDetailsFromFile: Calling readFile...");
-  fs.readFile(`./data/${breed}.txt`,"utf8",(error,data) => {
-    console.log("In readFile's Callback: it has the data");
-    //ISSUE: Returning from *inner* callback funciton, not breedDetailsFromFile.
-    if (!error) return data;
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) funcWhenThingsDone(data);
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+
 };
 
-// we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+const printBreed = breedData => {
+  console.log('Return Value: ', breedData)
+};
+
+breedDetailsFromFile("Bombay",printBreed);
